3698a2d8743b27cecce052929cc57f9f9620d868
Arshad Ali <thearshadali@yahoo.com>
2018-12-03 10:41:01 +0530

    file and folder

--- FilesAndFolderExample/ViewController.swift
+++ FilesAndFolderExample/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var fm : FileManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      
+    }
+    
+    @IBAction func createBtnAction(_ sender: UIButton) {
+        createNewFolder()
+    }
+    
+    func createNewFolder(){
+        let fm = FileManager.default
+        let docUlrs = fm.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        if let docUrl = docUlrs.first {
+          let newFolderUrl = docUrl.appendingPathComponent("newFolder")  // documentsDirectory/newFolder
+            // create new folder
+            do{
+                try fm.createDirectory(at: newFolderUrl, withIntermediateDirectories: false, attributes: nil)
+                 printFolderContents()
+            }catch{
+                print(error.localizedDescription)
+            }
+           
+        }
+       
     }
 
+    func printFolderContents(){
+           let fm = FileManager.default
+           let docUlr = try? fm.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        if docUlr != nil {
+          let results = try? fm.contentsOfDirectory(at: docUlr!, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+            
+            if results != nil {
+                for r in results!{
+                    print(r)
+                }
+            }
+        }
+        
+    }
+    
 
 }
 
